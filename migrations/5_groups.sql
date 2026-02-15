fd0f7440eb5b3c561139d507746fca38328e687a
Christian Knab <christiantknab@gmail.com>
2026-02-15 15:45:06 -0800

    remove media type from group storage

--- migrations/5_groups.sql
+++ migrations/5_groups.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS encrypted_group_states (
     id TEXT PRIMARY KEY,
 
-    -- The group identifier for a User's gropu snapshot
+    -- The group identifier for a User's group snapshot
     group_id UUID NOT NULL,
 
     -- The User who owns this encrypted state snapshot (only for the user to retrieve + update)
@@ -12,9 +12,6 @@ CREATE TABLE IF NOT EXISTS encrypted_group_states (
 
     -- The opaque encrypted blob containing the full group state
     encrypted_content BYTEA NOT NULL,
-
-    -- I was thinking we'd store these? but its useless with just Eko implementation
-    media_type TEXT NOT NULL DEFAULT 'application/eko-group-state',
     encoding TEXT NOT NULL DEFAULT 'base64',
 
     UNIQUE(user_id, group_id)
